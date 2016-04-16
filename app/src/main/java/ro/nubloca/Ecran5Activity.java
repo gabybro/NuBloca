83cb9cd10d69e9ff253c5308ad899564fdc9860e
gabybro <gaby_starnet@yahoo.com>
2016-04-16 10:15:55 +0300

    * fix

--- app/src/main/java/ro/nubloca/Ecran5Activity.java
+++ app/src/main/java/ro/nubloca/Ecran5Activity.java
@@ -1,14 +1,18 @@
 package ro.nubloca;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Ecran5Activity extends AppCompatActivity {
-
+    Button button, button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,40 @@ public class Ecran5Activity extends AppCompatActivity {
         getSupportActionBar().setTitle("                 Termeni si conditii");
         toolbar.setTitleTextColor(0xFFFFFFFF);
 
+
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                //Toast.makeText(Ecran5Activity.this, ":(", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(Ecran5Activity.this, Ecran4Activity.class));
+               // moveTaskToBack(true);
+                //android.os.Process.killProcess(android.os.Process.myPid());
+                //System.exit(1);
+            }
+        });
+
+        button1=(Button)findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                finish();
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+
     }
+
+
+
 
 }
