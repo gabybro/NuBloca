80b1ac94ae59fb20c02338c309a5886d28ca6b32
gabybro <gaby_starnet@yahoo.com>
2016-04-16 12:48:39 +0300

    * fix error

--- app/src/main/java/ro/nubloca/ScrollViewListener.java
+++ app/src/main/java/ro/nubloca/ScrollViewListener.java
@@ -1,8 +1,5 @@
 package ro.nubloca;
 
-/**
- * Created by PCPC on 13.4.2016.
- */
 public interface ScrollViewListener {
     void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
 
